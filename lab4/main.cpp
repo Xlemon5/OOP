e68be24dcd40f46808e24ce8a57cd3654bfc05d7
Xlemon5 <80254502+Xlemon5@users.noreply.github.com>
2023-10-31 09:14:21 +0300

    Update main.cpp

--- lab4/main.cpp
+++ lab4/main.cpp
@@ -1,12 +1,13 @@
-//#include <iostream>
+#include <iostream>
 #include "Pair.h"
 #include "Rational.h"
 using namespace std;
 
 
 
-int main() {
-
+int main()
+{
+    setlocale(LC_ALL, "ru");
     Pair p1;
     Pair p2;
 
@@ -16,33 +17,41 @@ int main() {
     cout << "Введите p2:" << endl;
     cin >> p2;
 
-    cout << p2 << endl;
 
-/*    // РЕЗУЛЬТАТ ВЫРАЖЕНИЯ - НОВАЯ ПАРА
+    //// РЕЗУЛЬТАТ ВЫРАЖЕНИЯ - НОВАЯ ПАРА
     cout << "Вычитание (p1 - p2): " << endl;
-    cout << p1 - p2 << endl;
+    Pair* vychit = p1 - p2;
+    cout << *vychit << endl;
 
     cout << "Сложение (p1 + p2): " << endl;
-    cout << p1 + p2 << endl;
+    Pair* sum = p1 + p2;
+    cout << *sum << endl;
 
     cout << "Умножение (p1 * p2): " << endl;
-    cout << p1 * p2 << endl;
+    Pair* ymnozh = p1 * p2;
+    cout << *ymnozh << endl;
+
+    cout << "Деление (p1 / p2): " << endl;
+    Pair* del = p1 / p2;
+    cout << *del << endl;
+
 
     cout << "Проверка на равенство (p1 == p2): " << endl;
     if(p1 == p2)
     {
-        cout << "Пары равны!" << endl;
+    	cout << "Пары равны!" << endl;
     }
     else
     {
-        cout << "Пары не равны!" << endl;
-    }*/
+    	cout << "Пары не равны!" << endl;
+    }
 
 
 
-/*
     //ТЕПЕРЬ РАБОТАЕМ С Rational
     cout << endl;
+
+
     cout << "ТЕПЕРЬ РАБОТАЕМ С Rational:" << endl;
     cout << endl;
 
@@ -56,15 +65,29 @@ int main() {
     cin >> r2;
 
     cout << "Новая операция сложения (r1 + r2): " << endl;
-    cout << r1 + r2 << endl;
+    Rational* sumr = r1 + r2;
+    cout << *sumr << endl;
 
     cout << "Переопределенная операция вычитания (r1 - r2): " << endl;
-    cout << r1 - r2 << endl;
+    Rational* vychitr = r1 - r2;
+    cout << *vychitr << endl;
 
     cout << "Новая операция деления (r1 / r2): " << endl;
-    cout << r1 / r2 << endl;
-*/
+    Rational* delr = r1/r2;
+    cout << *delr << endl;
 
+    cout << "Умнож (r1 * r2): " << endl;
+    Rational* ymnzhr = r1*r2;
+    cout << *ymnzhr << endl;
+
+    if (r1 > r2)
+    {
+        cout << "да" << endl;
+    }
+    else
+    {
+        cout << "нет" << endl;
+    }
 
 
     return 0;
