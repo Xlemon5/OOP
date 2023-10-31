64a48768e18d0f9bd6224af5b6ddcea9363aada9
Xlemon5 <80254502+Xlemon5@users.noreply.github.com>
2023-10-31 09:15:05 +0300

    Update Pair.h

--- lab4/Pair.h
+++ lab4/Pair.h
@@ -1,56 +1,49 @@
-#ifndef LAB4_PAIR_H
-#define LAB4_PAIR_H
+#ifndef LAB4_NEW_PAIR_H
+#define LAB4_NEW_PAIR_H
+
 #include <iostream>
+
 using namespace std;
 
-
 class Pair{
-public:
 
+public:
     int a;
     int b;
 
-    //конструкторы
-    Pair(int x = 0, int y = 0);
+    Pair();
+    Pair(int x, int y);
 
-
-    Pair operator * (const Pair &other) const;
-
-    Pair operator - (const Pair &other) const;
-
-    Pair operator + (const Pair &other) const;
-
-
-    // проверка на равенство
-    bool operator == (const Pair &other) const;
-
-    bool operator != (const Pair &other) const;
-
-    //ГЕТТЕРЫ
     int getA();
 
     int getB();
 
-    //СЕТТЕРЫ
     void setA(int A);
 
     void setB(int B);
 
-    //ОПЕРАТОР ПРИСВАИВАНИЯ
-    Pair& operator=(const Pair& other);
+    Pair& operator =(const Pair& other);
 
-    //ОПЕРАТОР КОПИРОВАНИЯ
-    Pair(const Pair&other);
+    Pair(const Pair& other);
 
+    bool operator ==(const Pair& other);
 
-    //перегрузка вывода
-    friend std::ostream& operator<<(std::ostream &out, const Pair& other);
+    bool operator !=(const Pair& other);
 
-    //перегрузка ввода
-    friend std::istream& operator >> (std::istream& in, Pair &other);
+    Pair* operator *(Pair& other) ;
 
+    Pair* operator +(Pair& other) ;
+
+    Pair* operator -(Pair& other) ;
+
+    Pair* operator /(Pair& other) ;
+
+    friend ostream& operator << (ostream& out, const Pair& other);
+
+    friend istream& operator >> (istream& in, Pair& other);
 
 };
 
 
-#endif //LAB4_PAIR_H
+
+#endif //LAB4_NEW_PAIR_H
